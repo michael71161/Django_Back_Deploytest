dc99f7c8ee5863439b9c75c20b491d56d29f0778
michael71161 <michael71161@gmail.com>
2022-11-26 18:22:07 +0200

    register all models

--- base/admin.py
+++ base/admin.py
@@ -1,8 +1,10 @@
 
 from django.contrib import admin
-from .models import Product
+from .models import Product, Order, Order_det
 # Register your models here.
 
 
 admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(Order_det)
 
